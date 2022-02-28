c5b15646dae0d1205962ef28a5d5c066bf7abfc9
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-02-28 14:24:40 -0300

    Create BookingService.java

---
+++ src/main/java/com/example/tdd/service/BookingService.java
@@ -0,0 +1,11 @@
+package com.example.tdd.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+
+    public int daysCalculatorWithDatabase(String name) {
+        return 10;
+    }
+}
